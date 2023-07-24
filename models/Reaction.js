1424647ec42c115e781c328ecc8da77d2b8b6714
Jeffrey V <jeffreyvicente@hotmail.com>
2023-07-23 17:53:12 -0700

    Created Reaction schema. Completed, no errors

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            minlength: 1,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => timestamp.toLocaleString(),
+        },
+    }
+);
+
+module.exports = reactionSchema;
