a1c238fa28897ace0b7e185360a893ff690808c4
Jeffrey V <jeffreyvicente@hotmail.com>
2023-07-25 16:52:09 -0700

    Added and created the thought and user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 
+const { 
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/userController')
 
+router.route('/').get(getUsers).post(createUser);
 
+router.route('/:studentId').get(getSingleUser).delete(deleteUser);
 
+router.route('/:studentId/reaction').post(addReaction);
 
+router.route('/:studentId/reaction/:reactionId').delete(removeReaction);
 
 
 module.exports = router;
