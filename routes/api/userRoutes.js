92677b1d1b359763b7c476b7b487078927909812
Jeffrey V <jeffreyvicente@hotmail.com>
2023-07-25 20:47:46 -0700

    Correct typos, routes are working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,15 +7,16 @@ const {
     deleteUser,
     addReaction,
     removeReaction,
+    updateUser,
 } = require('../../controllers/userController')
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:studentId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
-router.route('/:studentId/reaction').post(addReaction);
+router.route('/:userId/reaction').post(addReaction);
 
-router.route('/:studentId/reaction/:reactionId').delete(removeReaction);
+router.route('/:userId/reaction/:friendId').delete(removeReaction);
 
 
 module.exports = router;
