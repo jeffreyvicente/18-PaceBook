2ee2bbe9c7810b9c882850c3ecbc1634e2549a13
Jeffrey V <jeffreyvicente@hotmail.com>
2023-07-25 20:48:19 -0700

    Correct typos in the mongodb address

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const { connect, connection } = require('mongoose');
 
 // Connection string for MongoDB
-const connectionString = process.env.MONGDB_URI || 'mongdb://127.0.0.1:27017/pacebookDB';
+const connectionString = process.env.MONGDB_URI || 'mongodb://127.0.0.1:27017/pacebookDB';
 
 // Establishes a connection to the MongoDB
 connect(connectionString);
