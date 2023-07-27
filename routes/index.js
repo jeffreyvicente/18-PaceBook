6a45b74740214faa1325e9134e74d11f35938e90
Jeffrey V <jeffreyvicente@hotmail.com>
2023-07-27 00:06:02 -0700

    add comments to the file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+// Imports the required module to create a router
 const router = require('express').Router();
+
+// Imports the API routes module
 const apiRoutes = require('./api/');
-
-
 router.use('/api', apiRoutes);
 
+// If the request does not match any of the defined routes, this middleware will be executed
 router.use((req, res) => res.send('Wrong route!'));
 
+// Exports the router 
 module.exports = router;
