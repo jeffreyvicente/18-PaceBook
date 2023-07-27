1342de0f201a9f1535af1b426a2996558da5c2b6
Jeffrey V <jeffreyvicente@hotmail.com>
2023-07-27 00:03:26 -0700

    added comments to the code

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// Imports the required modules
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Getts the current working directory path
 const cwd = process.cwd();
 
+// Setts the default port to 3001 or the value specified in the environment variable
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,9 +16,12 @@ const activity = cwd.includes('01-Activities')
   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
+// Configures the middleware to parse incoming requests with JSON payloads
 app.use(express.json());
+// Configures the routes for the application
 app.use(routes);
 
+// Start the server and listen on the specified port
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for ${activity} running on port ${PORT}!`);
